debcaf67c2ade76be17e48b39bfc64947bd844f3
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 08:09:39 -0300

    feature: create create instrument routes

--- src/shared/http/routes/instruments.routes.ts
+++ src/shared/http/routes/instruments.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { CreateInstrumentController } from "../../../modules/instruments/useCases/createInstrument/CreateInstrumentController";
 
 import { ListInstrumentsController } from "../../../modules/instruments/useCases/listInstruments/ListInstrumentsController";
 
 const instrumentsRoutes = Router();
 
 const listInstrumentsController = new ListInstrumentsController();
+const createInstrumentController = new CreateInstrumentController();
 
 instrumentsRoutes.get("/", listInstrumentsController.handle);
+instrumentsRoutes.post("/", createInstrumentController.handle);
 
 export { instrumentsRoutes };
