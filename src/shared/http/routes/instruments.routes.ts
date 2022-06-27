bf89839bc6620d6712178b5ad645a72a0169c518
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 10:52:19 -0300

    feature: create delete instrument routes

--- src/shared/http/routes/instruments.routes.ts
+++ src/shared/http/routes/instruments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateInstrumentController } from "../../../modules/instruments/useCases/createInstrument/CreateInstrumentController";
+import { DeleteInstrumentController } from "../../../modules/instruments/useCases/deleteInstrument/DeleteInstrumentController";
 
 import { ListInstrumentsController } from "../../../modules/instruments/useCases/listInstruments/ListInstrumentsController";
 
@@ -7,8 +8,10 @@ const instrumentsRoutes = Router();
 
 const listInstrumentsController = new ListInstrumentsController();
 const createInstrumentController = new CreateInstrumentController();
+const deleteInstrumentController = new DeleteInstrumentController();
 
 instrumentsRoutes.get("/", listInstrumentsController.handle);
 instrumentsRoutes.post("/", createInstrumentController.handle);
+instrumentsRoutes.delete("/:id", deleteInstrumentController.handle);
 
 export { instrumentsRoutes };
