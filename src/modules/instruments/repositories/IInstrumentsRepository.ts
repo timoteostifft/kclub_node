39f933ad77453091d485a5f4d9ad7b5d837ec3ac
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 11:14:21 -0300

    refactor: implement update instrument dto on update instrument method

--- src/modules/instruments/repositories/IInstrumentsRepository.ts
+++ src/modules/instruments/repositories/IInstrumentsRepository.ts
@@ -1,9 +1,10 @@
+import { IUpdateInstrumentDTO } from "../dtos/IUpdateInstrumentDTO";
 import { Instrument } from "../prisma/entities/Instrument";
 
 interface IInstrumentsRepository {
   list(): Promise<Instrument[]>;
   create(name: string, amount: string): Promise<void>;
-  update(name: string, amount: string): Promise<void>;
+  update({ id, data }: IUpdateInstrumentDTO): Promise<Instrument>;
   delete(id: string): Promise<void>;
   findByName(name: string): Promise<Instrument | null>;
   findById(id: string): Promise<Instrument | null>;
