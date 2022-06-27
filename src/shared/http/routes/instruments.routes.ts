3a29d041cf1f23143198499bf4b442a73bd10fb0
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 11:15:00 -0300

    feature: create update instrument routes

--- src/shared/http/routes/instruments.routes.ts
+++ src/shared/http/routes/instruments.routes.ts
@@ -1,17 +1,20 @@
 import { Router } from "express";
+
 import { CreateInstrumentController } from "../../../modules/instruments/useCases/createInstrument/CreateInstrumentController";
 import { DeleteInstrumentController } from "../../../modules/instruments/useCases/deleteInstrument/DeleteInstrumentController";
-
 import { ListInstrumentsController } from "../../../modules/instruments/useCases/listInstruments/ListInstrumentsController";
+import { UpdateInstrumentController } from "../../../modules/instruments/useCases/updateInstrument/UpdateInstrumentController";
 
 const instrumentsRoutes = Router();
 
 const listInstrumentsController = new ListInstrumentsController();
 const createInstrumentController = new CreateInstrumentController();
 const deleteInstrumentController = new DeleteInstrumentController();
+const updateInstrumentController = new UpdateInstrumentController();
 
 instrumentsRoutes.get("/", listInstrumentsController.handle);
 instrumentsRoutes.post("/", createInstrumentController.handle);
 instrumentsRoutes.delete("/:id", deleteInstrumentController.handle);
+instrumentsRoutes.patch("/:id", updateInstrumentController.handle);
 
 export { instrumentsRoutes };
