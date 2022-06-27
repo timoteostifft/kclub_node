0320eb4a5a98fdbeaa0f2dbfeae870c8d33e1e2b
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 08:10:10 -0300

    feature: implement custom error message usage

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 
 import { router } from "./routes";
+import { AppError } from "../errors/AppError";
 
 import "../container";
 
@@ -11,5 +12,18 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`
+  })
+})
 
 app.listen(8080, () => console.log("Server is running!"));
