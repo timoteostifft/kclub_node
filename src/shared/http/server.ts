8a1d231e1e1bb3bf63a0d8bbade78e734b36f97b
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 02:40:23 -0300

    feature: add tsyringe container on server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,9 @@
+import "reflect-metadata";
+import express from "express";
+
 import { router } from "./routes";
 
-import express from "express";
+import "../container";
 
 const app = express();
 
