cbafba7e0880baf761323320631c61b68dff0afb
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 02:54:25 -0300

    feature: implement instruments routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import { instrumentsRoutes } from "./instruments.routes";
+
 const router = Router();
+
+router.use("/instruments", instrumentsRoutes);
 
 export { router };
