dfb1ee52663833aac0caff92fbf80a9fe86ae4cf
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-27 02:45:47 -0300

    feature: create instruments repository interface

---
+++ src/modules/instruments/repositories/IInstrumentsRepository.ts
@@ -0,0 +1,11 @@
+import { Instrument } from "../prisma/entities/Instrument";
+
+interface IInstrumentsRepository {
+  list(): Promise<Instrument[]>;
+  create(name: string, amount: string): Promise<void>;
+  update(name: string, amount: string): Promise<void>;
+  delete(id: string): Promise<void>;
+  findByName(name: string): Promise<Instrument | null>;
+}
+
+export { IInstrumentsRepository };
